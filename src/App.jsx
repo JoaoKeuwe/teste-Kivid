d291c55474fa553320010e10f5b8169deb65880b
JoaoKeuwe <joaov2872@hotmail.com>
2023-02-04 15:40:52 -0300

    feat: :fire: Adicionando a estrutura do site, contendo o card com as informaçaões da API

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,38 @@ function App() {
     }
   }
 
- 
+  return (
+    <div className="container">
+      <h1 className="title">Buscador de Cep</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>Rua: {cep.logradouro}</span>
+          <span>Bairro: {cep.bairro}</span>
+
+          <span>
+            Localidade: {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
+    </div>
+  );
+}
 
 export default App;
